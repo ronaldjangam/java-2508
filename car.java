2c100ddf2ae7e0ba9747f79e88abe4e76d303d8f
Ronald Gospel Jangam <jangamronaldgospel@gmail.com>
2025-06-11 08:56:19 +0530

    initial

---
+++ car.java
@@ -0,0 +1,23 @@
+public class car {
+    public static void main(String[] args) {
+        String carName = "Mahindra Thar";
+        float rating = 4.5f;
+        short numOfReviews = 1400;
+        int lowerPrice = 1150000;
+        int upperPrice = 1762000;
+        byte colors = 6;
+        byte images = 38;
+        short engineLowerCapacity = 1497;
+        short engineHigherCapacity = 2184; 
+        float powerLower = 116.93f;
+        float powerHigher = 150.19f;
+        byte seatingCapacity = 4;
+        byte globalNCAPSafetyRating = 4;
+        short groundClearance = 226;
+        short torqueLower = 300;
+        short torqueHigher = 320;
+        String driveType1 = "4WD";
+        String driveType2 = "RWD";
+    }
+    
+}
---
+++ myntra.java
@@ -0,0 +1,16 @@
+public class myntra {
+    public static void main(String[] args) {
+        String brandName = "HIGHLANDER";
+        String productName = "Men Comfort Relaxed Fit Textured Spread Collared Casual Shirt";
+        float rating = 4.6f;
+        byte numOfRatings = 5;
+        short originalMRP = 3949;
+        byte discount = 81;
+        byte sizeOption1 = 39;
+        byte sizeOption2 = 40;
+        byte sizeOption3 = 42;
+        byte sizeOption4 = 44;
+
+    }
+    
+}
