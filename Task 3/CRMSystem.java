f86e477e09f9fc3532b6109899f2d2d1e95afa85
ronaldjangam <95870447+ronaldjangam@users.noreply.github.com>
2025-06-28 13:14:54 +0000

    intial

--- Task 3/CRMSystem.java
+++ Task 3/CRMSystem.java
@@ -12,12 +12,6 @@ public class CRMSystem{
         String leadPhone = "9123456789";
         String leadSource = "Website Form";
 
-        int interactionId = 301;
-        int relatedCustomerId = 101;
-        String interactionType = "Email"; // Email, Call, Meeting
-        String interactionDate = "2025-06-15";
-        boolean isPositiveInteraction = true;
-
         int opportunityId = 401;
         int relatedLeadId = 201;
         String opportunityName = "Java Training Contract";
