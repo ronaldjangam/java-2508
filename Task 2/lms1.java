aff6248efd8cf7c0d0a641245f8b4e898dad028c
ronaldjangam <95870447+ronaldjangam@users.noreply.github.com>
2025-06-28 08:26:11 +0000

    initial

---
+++ Task 2/lms1.java
@@ -0,0 +1,25 @@
+public class lms1{
+    public static void main(String[] args){
+        int studentId = 101;
+        String firstName = "Ronald";
+        String lastName = "Jangam";
+        String email = "ronald.jangam@example.com";
+        String contactNumber = "9876543210";
+        String address = "123 Park Avenue, Telangana";
+        String dateOfBirth = "2006-04-29";
+        boolean isEnrolled = true;
+
+        int courseId = 301;
+        String courseName = "Introduction to Java";
+        String courseDescription = "Fundamentals of Java programming including OOP.";
+        int durationInWeeks = 12;
+
+        int trainerId = 501;
+        String trainerFirstName = "Ravi";
+        String trainerLastName = "Krishna";
+        String specialization = "Computer Science";
+        String trainerEmail = "ravi.krishna@gmail.com";
+
+
+    }
+}
---
+++ Task 2/studentForm.java
@@ -0,0 +1,17 @@
+public class studentForm{
+    public static void main(String[] args){
+        String lastName = "Jangam";
+        String firstName = "Ronald";
+        String middleName = "Gospel";
+        String completeAddress = "123 Park Avenue, 12345, Telangana";
+        String dateOfBirth = "04-29-2006";
+        String birthplace = "Guntur";
+        String citizenship = "Others";
+        String sex = "Male";
+        String civilStatus = "Single";
+        String contactNumber = "123-456-7890";
+        String fatherName = "Rajesh";
+        String fatherOccupation = "Engineer";
+        String fatherContactNumber = "987-654-3210";
+    }
+}
